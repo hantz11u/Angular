58e66be6ef47d351c8a4053d1929e652661cfe56
hantz11u <31924203+hantz11u@users.noreply.github.com>
2017-11-10 11:54:57 +0100

    Angular_Tour_Of_Heroes_Tp2
    code pour la résolution du point 2 (TheHeroesEditorl)

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
- 
+ 
 import { AppComponent }  from './app.component';
- 
+ 
 @NgModule({
   imports: [
     BrowserModule,
